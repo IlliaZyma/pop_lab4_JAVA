5b2141e95436c88e84b0fabaad01edf0473559bf
Illia Zyma <zyma.illia1121@vu.cdu.edu.ua>
2024-04-30 21:45:54 +0300

    fixed check for philosof.

--- src/Phylosopher.java
+++ src/Phylosopher.java
@@ -10,42 +10,33 @@ public class Phylosopher extends Thread{
     public void run() {
         int leftFork = id;
         int rightFork = (id + 1) % 5;
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Philosopher " + (id+1) + " thinking " + i + " time");
-
-            if (forks[leftFork].availablePermits() == 1 && forks[rightFork].availablePermits() == 1) {
-                try {
-                    forks[leftFork].acquire();
-                    System.out.println("Philosopher " + id + " took left fork");
-
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("Philosopher " + (id+1) + " thinking " + i + " time");
+                if(id % 2 == 0){
                     forks[rightFork].acquire();
                     System.out.println("Philosopher " + id + " took right fork");
-
-                    System.out.println("Philosopher " + id + " eating " + i + " time");
-
-                    forks[rightFork].release();
-                    System.out.println("Philosopher " + id + " put right fork");
-
-                    forks[leftFork].release();
-                    System.out.println("Philosopher " + id + " put left fork");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    forks[leftFork].acquire();
+                    System.out.println("Philosopher " + id + " took left fork");
                 }
-            } else {
-                while (true) {
-                    if (forks[leftFork].availablePermits() != 1 && forks[rightFork].availablePermits() != 1) {
-                        try {
-                            Thread.sleep(1);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        break;}
+                else {
+                    forks[leftFork].acquire();
+                    System.out.println("Philosopher " + id + " took left fork");
+                    forks[rightFork].acquire();
+                    System.out.println("Philosopher " + id + " took right fork");
                 }
+
+                System.out.println("Philosopher " + id + " eating " + i + " time");
+                sleep(1000);
+                forks[rightFork].release();
+                System.out.println("Philosopher " + id + " put right fork");
+
+                forks[leftFork].release();
+                System.out.println("Philosopher " + id + " put left fork");
+            }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
 
             }
         }
     }
-
-}
